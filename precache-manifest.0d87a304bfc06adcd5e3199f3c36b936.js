73d6c56c60f18f0da2d1c32a2718a590806414ab
Mikhail <mikhail.sapuzhak@gmail.com>
2020-04-20 21:02:37 +0300

    Updates

---
+++ precache-manifest.0d87a304bfc06adcd5e3199f3c36b936.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2c6d289955f774f6faf78f60911dc82",
+    "url": "/react-menu-app/index.html"
+  },
+  {
+    "revision": "8db407a06c254949e3ce",
+    "url": "/react-menu-app/static/css/main.38e76201.chunk.css"
+  },
+  {
+    "revision": "c5d9a36aa4b2a074f601",
+    "url": "/react-menu-app/static/js/2.27f6dab5.chunk.js"
+  },
+  {
+    "revision": "8db407a06c254949e3ce",
+    "url": "/react-menu-app/static/js/main.837ffb85.chunk.js"
+  },
+  {
+    "revision": "f5cfa5ade74ad0f09d2f",
+    "url": "/react-menu-app/static/js/runtime~main.e84d7fe9.js"
+  },
+  {
+    "revision": "698e5e807daf0026f6ac590ce288822c",
+    "url": "/react-menu-app/static/media/food-bg.698e5e80.jpg"
+  },
+  {
+    "revision": "701d6d1d57f5d528c4db1e887ff29e82",
+    "url": "/react-menu-app/static/media/shopping-cart-solid.701d6d1d.svg"
+  }
+]);
